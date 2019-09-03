19091c53ae32554c3ea5bd578b0f5960be1514c4
Patrick Young <patrick@blitzpredict.io>
2019-09-02 23:00:25 -0500

    Some is now async.

--- packages/async/src/some/some.ts
+++ packages/async/src/some/some.ts
@@ -1,4 +1,22 @@
-function some(array: Array<any>, predicate: Function): boolean {
+import isAsyncFunction from "../isAsyncFunction/isAsyncFunction";
+async function someAsync(array: Array<any>, predicate: Function): Promise<boolean> {
+    let index = -1;
+    const length = array == null ? 0 : array.length;
+
+    while (++index < length) {
+        if (await predicate(array[index], index, array)) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+function some(array: Array<any>, predicate: Function): Promise<boolean> | boolean {
+    if (isAsyncFunction(predicate)) {
+        return someAsync(array, predicate);
+    }
+
     let index = -1;
     const length = array == null ? 0 : array.length;
 
